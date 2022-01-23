d87db768810a32974f56949351fe5b597447f22a
kristian77010 <kristian77010@gmail.com>
2022-01-23 19:38:49 +0100

    yeet

--- html/app.js
+++ html/app.js
@@ -953,7 +953,7 @@ const vehHud = {
       this.showSquareB = data.showSquareB;
       this.showCircleB = data.showCircleB;
       this.speedType = data.speedType
-      console.log(toString(data.speedType))
+      console.log(data.speedType)
       if (data.speedType === true) {
         root.style.setProperty('--speedType', "MPH");
       } else {
